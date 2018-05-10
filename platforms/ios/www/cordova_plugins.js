02f2540b42a6572e17f73ddec66850ddb614a33d
Mike Brook <mkbrk@hotmail.com>
2018-05-10 07:46:27 -0800

    initial commit - working app

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,45 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "es6-promise-plugin.Promise",
+    "file": "plugins/es6-promise-plugin/www/promise.js",
+    "pluginId": "es6-promise-plugin",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-x-socialsharing.SocialSharing",
+    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+    "pluginId": "cordova-plugin-x-socialsharing",
+    "clobbers": [
+      "window.plugins.socialsharing"
+    ]
+  },
+  {
+    "id": "cordova-plugin-mapbox.Mapbox",
+    "file": "plugins/cordova-plugin-mapbox/www/Mapbox.js",
+    "pluginId": "cordova-plugin-mapbox",
+    "clobbers": [
+      "Mapbox"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "es6-promise-plugin": "4.2.2",
+  "cordova-plugin-x-socialsharing": "5.4.0",
+  "cordova-plugin-mapbox": "1.2.3"
+};
+// BOTTOM OF METADATA
+});
